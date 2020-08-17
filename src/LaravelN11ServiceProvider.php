48168e8492ba536bda05d09fdc2e14ae3c32b660
Onur Kaçmaz <kacmaz.onur@hotmail.com>
2020-08-18 00:05:36 +0300

    deleted comments

--- src/LaravelN11ServiceProvider.php
+++ src/LaravelN11ServiceProvider.php
@@ -11,33 +11,11 @@ class LaravelN11ServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-n11');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-n11');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('laravel-n11.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-n11'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-n11'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-n11'),
-            ], 'lang');*/
 
             // Registering package commands.
             // $this->commands([]);
