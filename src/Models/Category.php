866cd6d64cb4cee84563615948c49d9a578dcceb
Onur Kaçmaz <kacmaz.onur@hotmail.com>
2020-08-23 00:11:16 +0300

    add new method in category model

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -2,8 +2,6 @@
 
 namespace Onurkacmaz\LaravelN11\Models;
 
-
-use Illuminate\Support\Collection;
 use Onurkacmaz\LaravelN11\Exceptions\N11Exception;
 use Onurkacmaz\LaravelN11\Service;
 use SoapClient;
@@ -41,4 +39,69 @@ class Category extends Service
         return $this->_client->GetTopLevelCategories($this->_parameters);
     }
 
+    /**
+     * @param int $categoryId
+     * @param int $currentPage
+     * @param int $pageSize
+     * @return mixed
+     * @description İstenilen kategori, üst seviye kategori veya diğer seviye kategorilerden olabilir, bu kategorilere ait olan özelliklerin ve bu özelliklere ait değerlerin listelenmesi için kullanılan metottur.
+     */
+    public function getCategoryAttributes(int $categoryId, int $currentPage = 1, int $pageSize = 100) {
+        $this->_parameters["categoryId"] = $categoryId;
+        $this->_parameters["pagingData"] = [
+            "currentPage" => $currentPage,
+            "pageSize" => $pageSize,
+        ];
+        return $this->_client->GetCategoryAttributes($this->_parameters);
+    }
+
+    /**
+     * @param int $categoryId
+     * @return mixed
+     * @description İstenilen kategori, üst seviye kategori veya diğer seviye kategorilerden olabilir, bu kategorilere ait olan özelliklerin ve bu özelliklere ait değerlerin listelenmesi için kullanılan metottur.
+     */
+    public function getCategoryAttributesId(int $categoryId) {
+        $this->_parameters["categoryId"] = $categoryId;
+        return $this->_client->GetCategoryAttributesId($this->_parameters);
+    }
+
+    /**
+     * @param int $categoryProductAttrId
+     * @param int $currentPage
+     * @param int $pageSize
+     * @return mixed
+     * @description Özelliğe ait değerleri listeler
+     */
+    public function getCategoryAttributeValue(int $categoryProductAttrId, int $currentPage = 1, int $pageSize = 100) {
+        $this->_parameters["categoryProductAttributeId"] = $categoryProductAttrId;
+        $this->_parameters["pagingData"] = [
+            "currentPage" => $currentPage,
+            "pageSize" => $pageSize,
+        ];
+        return $this->_client->GetCategoryAttributeValue($this->_parameters);
+    }
+
+    /**
+     * @param int $categoryId
+     * @return mixed
+     * @description Kodu verilen kategorinin birinci seviye üst kategorilerine ulaşmak için bu metot kullanılmalıdır.
+     * İkinci seviye üst kategorilere ulaşmak için (Örn. “Deri ayakkabı -> Ayakkabı -> Giysi” kategori ağacında “Giysi “ bilgisi) birinci seviye üst kategorinin (Örn. Ayakkabı) kodu verilerek tekrar servis çağırılmalıdır.
+     * Sorgulanan kategori sistemde bulunamazsa ‘kategori bulunamadı’ hatası alınır. Eğer ilgili kategori herhangi bir üst kategoriye sahip değilse ”parentCategory” bilgisi response içerisinde yer almaz.
+     */
+    public function getParentCategory(int $categoryId) {
+        $this->_parameters["categoryId"] = $categoryId;
+        return $this->_client->GetParentCategory($this->_parameters);
+    }
+
+    /**
+     * @param int $categoryId
+     * @return mixed
+     * @description Kodu verilen kategorinin birinci seviye alt kategorilerine ulaşmak için bu metot kullanılmalıdır.
+     * İkinci seviye alt kategorilere ulaşmak için (Örn. “Giysi -> Ayakkabı -> Deri ayakkabı” kategori ağacında “Deri ayakkabı” bilgisi) birinci Seviye alt kategorinin (Örn. Ayakkabı) kodu verilerek tekrar servis çağırılmalıdır.
+     * Sorgulanan kategori sistemde bulunamazsa hata alınır. Eğer ilgili kategori herhangi bir alt kategoriye sahip değilse response bilgisi boş döner.
+     */
+    public function getSubCategories(int $categoryId) {
+        $this->_parameters["categoryId"] = $categoryId;
+        return $this->_client->GetSubCategories($this->_parameters);
+    }
 }
