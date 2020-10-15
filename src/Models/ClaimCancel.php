16bc4fda071d112267e58ce0791d5e3e9db5f9f1
Onur Kaçmaz <kacmaz.onur@hotmail.com>
2020-10-15 23:26:39 +0300

    added comments

--- src/Models/ClaimCancel.php
+++ src/Models/ClaimCancel.php
@@ -26,11 +26,21 @@ class ClaimCancel extends Service implements ClaimCancelInterface
         $this->_client = $this->setEndPoint(self::END_POINT);
     }
 
+    /**
+     * @param array $data
+     * @return object
+     */
     public function list(array $data): object {
         $this->_parameters["searchData"] = $data;
         return $this->_client->ClaimCancelList($this->_parameters);
     }
 
+    /**
+     * @param int $claimCancelId
+     * @param int $reasonId
+     * @param string $note
+     * @return object
+     */
     public function deny(int $claimCancelId, int $reasonId, string $note): object {
         $this->_parameters["claimCancelId"] = $claimCancelId;
         $this->_parameters["denyReasonId"] = $reasonId;
@@ -38,10 +48,17 @@ class ClaimCancel extends Service implements ClaimCancelInterface
         return $this->_client->ClaimCancelDeny($this->_parameters);
     }
 
+    /**
+     * @return object
+     */
     public function denyReasonTypes(): object {
         $this->_client->ClaimCancelDenyReasonType($this->_parameters);
     }
 
+    /**
+     * @param int $claimCancelId
+     * @return object
+     */
     public function approve(int $claimCancelId): object {
         $this->_parameters["claimCancelId"] = $claimCancelId;
         $this->_client->ClaimCancelApprove($this->_parameters);
